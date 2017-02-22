5d9cb0c7366f35772a450e59d44dcdd3e25accbf
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2017-02-21 20:55:35 -0500

    last commit

---
+++ requestHandlers.js
@@ -0,0 +1,9 @@
+function start () {
+	console.log('Request Handler start was called.');
+}
+function upload () {
+	console.log('Requet Handler upload was called.');
+}
+
+exports.start=start;
+exports.upload=upload;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start () {
+function start (route, handle) {
 	// body... 
 
 	function onRequest (req, res) {
 		var pathname=url.parse(req.url).pathname;
 		console.log('Request for '+pathname+' received.');
+		
+		route(handle, pathname);
+
 		res.writeHead(200, {"Content-Type": "text/plain"});
 		res.write("Hello world!");
 		res.end();
